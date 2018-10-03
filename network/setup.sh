40304c1170db4b325a864f0f2c7e12108aba8185
Giordano Rodrigues <giordano2dcc@gmail.com>
2018-10-03 11:39:10 -0300

    add network configuration dotfiles and setup scripts

---
+++ network/setup.sh
@@ -0,0 +1,16 @@
+#!/use/bin/env bash
+
+
+# copy the configuration file to its folder
+cp ./wpa_supplicant.conf /etc/wpa_supplicant/wpa_supliccant.conf
+
+# kill all instances of wpa_supplicant for not having conflicts
+killall wpa_supplicant
+
+# setup the configuration file to wlp2s0 interface
+wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+
+# run wpa_cli for estabilish connection with your network
+
+# (if not estabilished an ip address run dhclient)
+# dhclient wlp2s0
