bd5535665fa112676b0643451e0d8768f58728b1
Giordano Rodrigues <giordano2dcc@gmail.com>
2018-10-08 02:11:50 -0300

    add verbose flags

--- network/setup.sh
+++ network/setup.sh
@@ -4,7 +4,7 @@
 # cp ./wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf --verbose --backup
 
 # kill all instances of wpa_supplicant for not having conflicts
-killall wpa_supplicant
+killall wpa_supplicant --verbose
 
 # setup the configuration file to wlp2s0 interface
 wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
@@ -12,4 +12,4 @@ wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf
 # run wpa_cli for estabilish connection with your network
 
 # (if not estabilished an ip address run dhclient)
-dhclient wlp2s0
+dhclient wlp2s0 -v
