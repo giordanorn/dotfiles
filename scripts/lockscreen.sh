3628ad43f9702b1eab50c1071907b1199dfab1aa
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-05-16 23:35:13 -0300

    improvements on functionality and styling

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
 
 # get a random file
-#WALLPAPER=$(find /usr/local/share/backgrounds | shuf | head -n1) 
-WALLPAPER=/usr/local/share/backgrounds/01-bLxcjh3.png
+WALLPAPER=$(find /usr/local/share/backgrounds | shuf | head -n1) 
 
-pgrep -x i3lock || i3lock --clock --datestr="%A %B %e" --line-uses-inside --image=$WALLPAPER
+# suspend notification daemon
+pkill -STOP dunst
+
+pgrep -x i3lock || \
+	i3lock \
+		--force-clock \
+		--datestr="%A %B %e" \
+		--line-uses-inside \
+		--image=$WALLPAPER \
+		--ignore-empty-password \
+		--indicator \
+		--insidecolor=282828ff \
+		--timecolor=eeeeeeff \
+		--datecolor=eeeeeeff \
+		--ringcolor=f22c40ff \
+		--keyhlcolor=eeeeeeff \
+		--bshlcolor=282828ff \
+		--separatorcolor=eeeeeeff \
+		--time-font='Open Sans:style=Bold' \
+		--date-font='Open Sans:style=Bold' \
+		--timesize=32 \
+		--datesize=16 \
+		--ring-width=4 \
+		--radius=120 \
+		--veriftext="" \
+		--wrongtext="" \
+		--noinputtext="" \
+
+# resume notification daemon
+pkill -CONT dunst
