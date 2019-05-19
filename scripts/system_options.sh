ba6b65d7b64ea0e5690a369ebaeeec28a88b0fdd
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-05-19 00:35:10 -0300

    using a single script for managing system options like shutdown, logout,

---
+++ scripts/system_options.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+OPTION=`echo -e "Shutdown\nReboot\nHibernate\nSuspend\nLogout\nNevermind" | dmenu -i -p "What do you want to do?"`
+
+CONFIRM=$HOME/scripts/confirm.sh
+
+case $OPTION in
+	"Shutdown")
+		$CONFIRM "Are you sure you want to shutdown now?" && systemctl poweroff ;;
+	"Reboot")
+		$CONFIRM "Are you sure you want to reboot?" && systemctl reboot ;;
+	"Hibernate")
+		$CONFIRM "Are you sure you want to hibernate?" && systemctl hibernate ;;
+	"Suspend")
+		$CONFIRM "Are you sure you want to suspend" && systemctl suspend ;;
+	"Logout")
+		$CONFIRM "Are you sure you want to end your session" && i3-msg exit ;;
+	*)
+		exit 1 ;;
+esac
+
+
