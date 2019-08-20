556470b4e0db320f350f1936598dfe45940c5081
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-08-20 00:03:12 -0300

    some general fixes

--- apply.sh
+++ apply.sh
@@ -3,24 +3,24 @@
 # apply all files from dotfiles to its correspondent folders in your home directory
 
 # catch all files in repo
-files=`find -mindepth 1 $SCRITPS -type f | grep -v '.git\/\|.md\|pull.sh\|apply.sh'`
+files=$(find . -mindepth 1 -type f | grep -v '.git\/\|.md\|pull.sh\|apply.sh')
 
 # diff all of them with correspondent local file on machine
 for file in $files
 do 
 	relative=${file#"./"}
-	dotfile=$(pwd)/$relative
-	userfile=$HOME/$relative
+	dotfile="$(pwd)/$relative"
+	userfile="$HOME/$relative"
 	echo "Applying diff $dotfile -> $userfile..."
-	diff --color=auto -N $userfile $dotfile
-	if [ $? -eq 0 ]; then
+	
+	if diff --color=auto -N "$userfile" "$dotfile"; then
 		echo "File is up to date."
 	else
-		echo -n "Files have been changed. Do you want to apply (dotfiles -> $USER)? (y/N) "
-		read answer
-		if [ "$answer" != "${answer#[Yy]}" ] ;then
+		printf "Files have been changed. Do you want to apply (dotfiles -> %s)? (y/N) " "$USER"
+		read -r answer
+		if [ ! "$answer" = "${answer#[Yy]}" ] ;then
 			echo Copying file...
-			cp $dotfile $userfile
+			cp "$dotfile" "$userfile"
 			echo Done.
 		else
 			echo Skipping.
--- pull.sh
+++ pull.sh
@@ -3,24 +3,23 @@
 # import local files from your computer to its correspondents dotfiles folder
 
 # catch all files in repo
-files=`find -mindepth 1 $SCRITPS -type f | grep -v '.git\/\|.md\|pull.sh\|apply.sh'`
+files=$(find . -mindepth 1 -type f | grep -v '.git\/\|.md\|pull.sh\|apply.sh')
 
 # diff all of them with correspondent local file on machine
 for file in $files
 do 
 	relative=${file#"./"}
-	dotfile=$(pwd)/$relative
-	userfile=$HOME/$relative
+	dotfile="$(pwd)/$relative"
+	userfile="$HOME/$relative"
 	echo "Applying diff $userfile -> $dotfile..."
-	diff --color=auto -N $dotfile $userfile
-	if [ $? -eq 0 ]; then
+	if diff --color=auto -N "$dotfile" "$userfile"; then
 		echo "File is up to date."
 	else
-		echo -n "Files have been changed. Do you want to apply ($USER -> dotfiles)? (y/N) "
-		read answer
+		printf "Files have been changed. Do you want to apply (%s -> dotfiles)? (y/N) " "$USER"
+		read -r answer
 		if [ "$answer" != "${answer#[Yy]}" ] ;then
 			echo Copying file...
-			cp $userfile $dotfile
+			cp "$userfile" "$dotfile"
 			echo Done.
 		else
 			echo Skipping.
