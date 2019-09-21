f26f54a45fda6efe0c0d01ccd53eb5101d8d4dac
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-09-20 23:56:15 -0300

    minor fixes

--- scripts/system-options.sh
+++ scripts/system-options.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-OPTION=$(printf "Shutdown\nReboot\nHibernate\nSuspend\nLogout\nNevermind" | dmenu -i -p "What do you want to do?")
+option=$(printf "Shutdown\nReboot\nHibernate\nSuspend\nLogout\nNevermind" | dmenu -i -p "What do you want to do?")
+confirm="sh $SCRIPTS/confirm.sh"
 
-CONFIRM="sh $HOME/scripts/confirm.sh"
-
-case $OPTION in
+case $option in
 	"Shutdown")
-		$CONFIRM "Are you sure you want to shutdown now?" && systemctl poweroff ;;
+		$confirm "Are you sure you want to shutdown now?" && systemctl poweroff ;;
 	"Reboot")
-		$CONFIRM "Are you sure you want to reboot?" && systemctl reboot ;;
+		$confirm "Are you sure you want to reboot?" && systemctl reboot ;;
 	"Hibernate")
-		$CONFIRM "Are you sure you want to hibernate?" && systemctl hibernate ;;
+		$confirm "Are you sure you want to hibernate?" && systemctl hibernate ;;
 	"Suspend")
-		$CONFIRM "Are you sure you want to suspend" && systemctl suspend ;;
+		$confirm "Are you sure you want to suspend" && systemctl suspend ;;
 	"Logout")
-		$CONFIRM "Are you sure you want to end your session" && i3-msg exit ;;
+		$confirm "Are you sure you want to end your session" && i3-msg exit ;;
 	*)
 		exit 1 ;;
 esac
